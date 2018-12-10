2980f63ffdb535b3e820c69b4f9906ec72025d6a
GrazzyDuck <lepelaar.bas@gmail.com>
2018-12-10 18:40:38 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
-const token = process.env.token;
 
-const bot = new Discord.Client();
+const bot = new Discord.Client
 
 bot.on("ready", async () => {
     console.log(`${bot.user.username} is online!`);
